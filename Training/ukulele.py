b963cd30923f3180d9e2c261c75eb3381fb4083e
Arseniy <dsxcvfrewqaz@gmail.com>
2018-01-31 23:58:28 +0300

    First commit

--- Training/ukulele.py
+++ Training/ukulele.py
@@ -1,2 +1,14 @@
-a = "hiu"
-print(a)
+def solution(number):
+    i = 0
+    j = 0
+    while i < number:
+        if i % 3 == 0:
+            j = j + i
+
+        elif i % 5 == 0:
+            j = j + i
+        i += 1
+    return j
+
+
+print(solution(10))
