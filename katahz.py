b92491d768713dbdbc020192f05a02119bf7a19c
Arseniy <dsxcvfrewqaz@gmail.com>
2018-03-28 08:08:04 +0300

    First commit

--- katahz.py
+++ katahz.py
@@ -1,6 +1,5 @@
 listre = [{'name': 'govnoed'}, {'name': 'andrey'}, {'name': 'sergey'}, {'name': 'hodor'}]
 
-
 print(listre[-1])
 
 
@@ -11,10 +10,15 @@ def namelist(names):
     if len(a) <= 2:
         return ' & '.join(a[len(a) - 2:])
     else:
-        return (', '.join(a[0:len(a) - 2]) + ', ' + ' & '.join(a[len(a) - 2:]))
+        return ', '.join(a[0:len(a) - 2]) + ', ' + ' & '.join(a[len(a) - 2:])
 
-def namelist(names):
-    if len(names)==0: return ''
-    if len(names)==1: return names[0]['name']
+
+def namelist1(names):
+    if len(names) == 0:
+        return ''
+    if len(names) == 1:
+        return names[0]['name']
     return ', '.join([n['name'] for n in names[:-1]]) + ' & ' + names[-1]['name']
 
+
+print(namelist1(listre))
